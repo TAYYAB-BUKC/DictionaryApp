2056bd3e5099df5df8353aeee6dc7c1d2fb6078f
TAYYAB DOST <tayyabdost@gmail.com>
2019-07-12 08:55:37 +0500

    Designing the WordMeaningActivity

---
+++ app/src/main/java/com/example/dictionaryapp/WordMeaningActivity.java
@@ -0,0 +1,33 @@
+package com.example.dictionaryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class WordMeaningActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_meaning);
+
+        toolbar = (Toolbar) findViewById(R.id.wordMeaningtoolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("English Words");
+
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        if(item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
