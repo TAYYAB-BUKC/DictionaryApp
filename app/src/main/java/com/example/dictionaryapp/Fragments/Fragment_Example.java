730e521b667e5e24cf8de49d43cda4a8e95ba177
TAYYAB DOST <tayyabdost@gmail.com>
2019-07-12 09:11:35 +0500

    Adding Fragments to the WordMeaningActivity

---
+++ app/src/main/java/com/example/dictionaryapp/Fragments/Fragment_Example.java
@@ -0,0 +1,25 @@
+package com.example.dictionaryapp.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dictionaryapp.R;
+
+public class Fragment_Example extends Fragment {
+    public Fragment_Example() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragments_definition,container, false);//Inflate Layout
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/dictionaryapp/WordMeaningActivity.java
+++ app/src/main/java/com/example/dictionaryapp/WordMeaningActivity.java
@@ -23,8 +23,6 @@ public class WordMeaningActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-
         if(item.getItemId() == android.R.id.home) {
             onBackPressed();
         }
