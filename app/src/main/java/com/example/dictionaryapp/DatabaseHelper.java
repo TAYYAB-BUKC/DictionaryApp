b416835ae5fb8d0785433790360ca8a5ccfd16d9
TAYYAB DOST <tayyabdost@gmail.com>
2019-07-12 16:03:50 +0500

    Adding Database Helper Class

--- app/src/main/java/com/example/dictionaryapp/DatabaseHelper.java
+++ app/src/main/java/com/example/dictionaryapp/DatabaseHelper.java
@@ -1,9 +1,17 @@
 package com.example.dictionaryapp;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
@@ -26,6 +34,79 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        try {
+            this.getReadableDatabase();
+            myContext.deleteDatabase(DB_NAME);
+            copyDataBase();
+        } catch (IOException e) {
+            e.printStackTrace();
 
+        }
+    }
+
+    private void copyDataBase() throws IOException {
+
+        InputStream myInput = myContext.getAssets().open(DB_NAME);
+        String outFileName = DB_PATH + DB_NAME;
+        OutputStream myOutput = new FileOutputStream(outFileName);
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer)) > 0) {
+            myOutput.write(buffer, 0, length);
+        }
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+
+        // Log.i("copyDataBase", "Database copied");
+    }
+
+
+    public boolean checkDataBase() {
+
+        SQLiteDatabase checkDB = null;
+        try {
+            String myPath = DB_PATH + DB_NAME;
+            checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+
+        } catch (SQLiteException e) {
+            //
+        }
+        if (checkDB != null) {
+            checkDB.close();
+        }
+
+        return checkDB != null ? true : false;
+    }
+
+
+    public void openDataBase() throws SQLException {
+        String myPath = DB_PATH + DB_NAME;
+        myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
+
+    }
+
+
+    @Override
+    public synchronized void close() {
+        if (myDataBase != null)
+            myDataBase.close();
+        super.close();
+    }
+
+    public void createDataBase() throws IOException {
+        boolean dbExist = checkDataBase();
+        if (!dbExist) {
+
+            this.getReadableDatabase();
+            try {
+                myContext.deleteDatabase(DB_NAME);
+                copyDataBase();
+
+            } catch (IOException e) {
+                throw new Error("Error copying database");
+            }
+
+        }
     }
 }
