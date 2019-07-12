b4083282fdf8bde1ed885328f29819961ac44461
TAYYAB DOST <tayyabdost@gmail.com>
2019-07-12 10:45:19 +0500

    Adding Database

---
+++ app/src/main/java/com/example/dictionaryapp/DatabaseHelper.java
@@ -0,0 +1,31 @@
+package com.example.dictionaryapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private String DB_PATH = null;
+    private static String DB_NAME = "Eng_Dictionary.db";
+    private SQLiteDatabase myDataBase;
+    private final Context myContext;
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, 1);
+        this.myContext = context;
+        this.DB_PATH = "/data/data/" + context.getPackageName() + "/" + "databases/";
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
